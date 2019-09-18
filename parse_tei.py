39884bdf8186249f3036bdfd5512e4dd68b6f7f8
seanrife <seanrife@gmail.com>
2019-09-17 21:46:44 -0500

    Initial commit of tei parser.

---
+++ parse_tei.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import os
+
+data_dir = "/home/sean/jayne/docs/naser/"
+
+
+def get_grafs(xml_input):
+    grafs_list = []
+    soup = BeautifulSoup(xml_input, "lxml")
+    grafs = soup.find_all('p')
+    for graf in grafs:
+        # TODO: clean the output and remove tags
+        # DFL
+        graf = graf.get_text()
+        grafs_list.append(graf)
+    return grafs
+
+
+def generate_filelist(data_dir):
+    file_list = []
+    for root, subdirs, files in os.walk(os.path.abspath(data_dir)):
+        for file in files:
+            if file[-3:] == "xml":
+                file_list.append(os.path.join(root, file))
+    return file_list
+
+
+file_list = generate_filelist(data_dir)
+
+for file in file_list:
+    with open(file, 'r') as f:
+        data = f.read()
+    grafs = get_grafs(data)
+    print(grafs)
