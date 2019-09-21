68060838499ebb2fc85f42cff0c926b524a6832e
seanrife <seanrife@gmail.com>
2019-09-20 21:37:55 -0500

    Added unified results printout function.

---
+++ printout.py
@@ -0,0 +1,7 @@
+def display(graf1, graf2, score, score_type):
+    print("================================================================================")
+    print(score_type + ": " + str(score))
+    print(" --- 1 --- ")
+    print(graf1 + "\n")
+    print(" --- 2 --- ")
+    print(graf2 + "\n\n")
